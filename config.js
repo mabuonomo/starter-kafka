fde31295fa33c0edcce3a73fe07f69fcf6087c5c
Mario Buonomo <ma.buonomo@gmail.com>
2019-02-05 22:26:07 +0100

    improve

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
   KAFKA_BROKER_HOST: 'kafka:2181',
   KAFKA_HOST: 'kafka:9092',
-  TOPIC_INITIAL : 'topic_initial',
-  TOPIC_FINAL: 'topic_final'
+  TOPIC_INITIAL: 'topic_initial',
+  TOPIC_FINAL: 'topic_final',
+  CONSUMER_GROUP_ID: 'ExampleTestGroup',
 };
--- src/options/optionsConsumer.ts
+++ src/options/optionsConsumer.ts
@@ -1,9 +1,9 @@
 import { ConsumerGroupStreamOptions } from "kafka-node";
-const { KAFKA_HOST } = require('../../config');
+const { KAFKA_HOST, CONSUMER_GROUP_ID } = require('../../config');
 
 export const consumerOptions: ConsumerGroupStreamOptions = {
     kafkaHost: KAFKA_HOST,
-    groupId: 'ExampleTestGroup',
+    groupId: CONSUMER_GROUP_ID,
     sessionTimeout: 15000,
     protocol: ['roundrobin'],
     id: 'consumer1',
